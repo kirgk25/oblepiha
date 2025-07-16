dd098aabeb81a386aa4ac4eb9769a701079732b4
Kirill Goroshko <kirgk25@gmail.com>
2025-07-16 20:55:25 +0300

    config cs fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,14 +1,18 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
+declare(strict_types=1);
+
+$finder = new PhpCsFixer\Finder()
     ->in(__DIR__ . '/app')
     ->in(__DIR__ . '/database')
     ->in(__DIR__ . '/routes')
     ->in(__DIR__ . '/tests');
 
-return (new PhpCsFixer\Config())
+return new PhpCsFixer\Config()
+    ->setRiskyAllowed(true)
     ->setRules([
         '@PER-CS' => true,
         '@PHP84Migration' => true,
+        'declare_strict_types' => true,
     ])
     ->setFinder($finder);
